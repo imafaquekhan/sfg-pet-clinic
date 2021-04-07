0181c13ef9e1347275f48cf2255adbb70eefe428
mak199 <mak.khan199@googlemail.com>
2021-04-07 11:07:03 +0200

    adding base entity with id. closes #10

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/VetService.java
@@ -1,7 +1,5 @@
 package com.springframework.sfgpetclinic.services;
 
-import com.springframework.sfgpetclinic.model.Owner;
-import com.springframework.sfgpetclinic.model.Pet;
 import com.springframework.sfgpetclinic.model.Vet;
 
 import java.util.Set;
