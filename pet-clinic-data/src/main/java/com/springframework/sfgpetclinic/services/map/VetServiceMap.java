ea0968525acef6c38f9e105fbc688fcaca192f1e
mak199 <mak.khan199@googlemail.com>
2021-04-08 14:24:17 +0200

    adding Spring Config. closes #22

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,13 +1,12 @@
 package com.springframework.sfgpetclinic.services.map;
 
-import com.springframework.sfgpetclinic.model.Owner;
-import com.springframework.sfgpetclinic.model.Pet;
 import com.springframework.sfgpetclinic.model.Vet;
-import com.springframework.sfgpetclinic.services.CrudService;
 import com.springframework.sfgpetclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
