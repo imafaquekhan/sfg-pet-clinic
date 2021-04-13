dc8ad18dd389bac54444bc6ff48b75510ae10d7c
mak199 <mak.khan199@googlemail.com>
2021-04-13 15:57:01 +0200

    Added Contact and Pets Info to Owner at startup. closes #33

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,25 @@
 package com.springframework.sfgpetclinic.services.map;
 
 import com.springframework.sfgpetclinic.model.Owner;
+import com.springframework.sfgpetclinic.model.Pet;
 import com.springframework.sfgpetclinic.services.OwnerService;
+import com.springframework.sfgpetclinic.services.PetService;
+import com.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
+import javax.print.attribute.standard.RequestingUserName;
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -31,7 +43,31 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner owner) {
-        return super.save(owner);
+        if(owner!=null){
+            if(owner.getPets()!=null){
+                owner.getPets().forEach(pet->{
+                    if(pet.getPetType()!=null){
+                        if(pet.getPetType().getId()==null){
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    }
+                    else{
+                        throw new RuntimeException("PetType is required.");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(owner);
+        }
+        else{
+            return null;
+        }
+
+
     }
 
 
--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.springframework.sfgpetclinic.bootstrap;
 
 import com.springframework.sfgpetclinic.model.Owner;
+import com.springframework.sfgpetclinic.model.Pet;
 import com.springframework.sfgpetclinic.model.PetType;
 import com.springframework.sfgpetclinic.model.Vet;
 import com.springframework.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.springframework.sfgpetclinic.services.PetTypeService;
 import com.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 // becomes a spring bean
 @Component
@@ -38,14 +41,29 @@ public class DataLoader implements CommandLineRunner {
         Owner owner = new Owner();
         owner.setFirstName("Micheal");
         owner.setLastName("Weston");
+
+        Pet pet = new Pet();
+        pet.setPetType(savedCatPetType);
+        pet.setOwner(owner);
+        pet.setBirthDate(LocalDate.now().minusMonths(5));
+        pet.setName("Rosco");
+        owner.getPets().add(pet);
         ownerService.save(owner);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+
+        Pet pet2 = new Pet();
+        pet2.setPetType(savedDogPetType);
+        pet2.setOwner(owner2);
+        pet2.setBirthDate(LocalDate.now().minusMonths(15));
+        pet2.setName("Sheru");
+        owner2.getPets().add(pet2);
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners");
+
 
         Vet vet = new Vet();
         vet.setFirstName("Sam");
