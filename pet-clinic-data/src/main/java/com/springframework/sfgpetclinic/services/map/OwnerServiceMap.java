67e5e920aab947a50df1a89053be50c7486e00c1
mak199 <mak.khan199@googlemail.com>
2021-04-27 15:52:32 +0200

    implementing find owner, added tests, and added method in services and controller. closes #54

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import javax.print.attribute.standard.RequestingUserName;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -79,5 +80,11 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
                 .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String firstName) {
+        // todo
+        return null;
     }
 }
